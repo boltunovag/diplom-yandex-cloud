d03cf174676f1fc622e91aac6bc49e327e436c48
Alex Boltunov <boltunovag@gmail.com>
2025-11-20 01:22:46 +0300

    Добавил filebeat-webservers.yml

--- deploy-from-bastion.sh
+++ deploy-from-bastion.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-# deploy-from-bastion.sh
-
 set -e
-
 BASTION_IP=$(terraform output -raw bastion_external_ip)
-
 echo "🚀 Deploying from bastion ($BASTION_IP)..."
 
 # Создаем структуру папок на бастионе
 ssh -i ~/.ssh/yc-ed25519 ubuntu@$BASTION_IP "mkdir -p ~/Diplom"
 
-# Копируем ТОЛЬКО папку ansible
+# Копируем  папку ansible
 rsync -avz -e "ssh -i ~/.ssh/yc-ed25519" \
   ./ansible/ ubuntu@$BASTION_IP:~/Diplom/ansible/
 
@@ -20,16 +16,27 @@ scp -i ~/.ssh/yc-ed25519 ~/.ssh/yc-ed25519 ubuntu@$BASTION_IP:~/.ssh/
 # Запускаем деплой
 ssh -i ~/.ssh/yc-ed25519 -o StrictHostKeyChecking=no ubuntu@$BASTION_IP << 'EOF'
 set -e
-
 echo "🔧 Setting up bastion environment..."
 chmod 600 ~/.ssh/yc-ed25519
-
 cd ~/Diplom/ansible
 
-echo "🎯 Running deployment..."
+echo "📦 Installing web servers..."
 ansible-playbook setup-webservers.yml
-ansible-playbook zabbix-setup.yml  
+
+echo "📊 Installing Zabbix Server..."
+ansible-playbook zabbix-setup.yml
+
+echo "🔍 Installing Zabbix Agents..."
 ansible-playbook zabbix-agents.yml
+
+echo "🔍 Installing Elasticsearch..."
+ansible-playbook elasticsearch-setup.yml
+
+echo "👁️  Installing Kibana..."
+ansible-playbook kibana-setup.yml
+
+echo "Installing FileBeat..." 
+ansible-playbook filebeat-webservers.yml
 
 echo "✅ Deployment completed!"
 EOF
